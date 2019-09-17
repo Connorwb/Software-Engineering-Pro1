159c021f4197274e0a740abc85b44e2211dfbabb
Connorwb <41408864+Connorwb@users.noreply.github.com>
2019-09-17 14:10:41 -0400

    Add files via upload

---
+++ Assets/BackGroundScrollerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScrollerScript : MonoBehaviour {
+
+    float scrollSpeed = -15f;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 20);
+        transform.position = startPos + Vector2.right * newPos;
+	}
+}
